cedb15e757c9d87d763120a457ebd2fb03cec9b1
mossadhagos <155261758+mossadhagos@users.noreply.github.com>
2025-12-15 15:00:52 +0100

    commit

--- sql/ingest_data.sql
+++ sql/ingest_data.sql
@@ -1,7 +1,7 @@
-CREATE TABLE 
+CREATE TABLE
     IF NOT EXISTS videos AS (
-        SELECT 
-            * 
-        FROM 
-            read_csv_auto('data/aigineer_yt_2024_2025.csv')
-    )
+        SELECT
+            *
+        FROM
+            read_csv_auto ('data/aigineer_yt_2024_2025.csv')
+    );
