5054003b023ec596ea51bc47623581e314852adf
mossadhagos <155261758+mossadhagos@users.noreply.github.com>
2025-12-15 14:40:31 +0100

    Update ingest_data.sql

--- sql/ingest_data.sql
+++ sql/ingest_data.sql
@@ -1,7 +1,1 @@
-CREATE TABLE 
-    IF NOT EXISTS videos AS (
-        SELECT 
-            * 
-        FROM 
-            read_csv_auto('data/aigineer_yt_2024_2025.csv')
-    )
+CREATE TABLE IF NOT EXISTS videos AS (SELECT * FROM read_csv_auto ('data/aigineer_yt_2024_2025.csv'))
