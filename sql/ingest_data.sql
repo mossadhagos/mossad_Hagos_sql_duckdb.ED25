b9e719433567e67e19618e637a7e3769b9d07b04
mossadhagos <155261758+mossadhagos@users.noreply.github.com>
2025-12-16 18:16:38 +0100

    commit

--- sql/ingest_data.sql
+++ sql/ingest_data.sql
@@ -3,5 +3,5 @@ CREATE TABLE
         SELECT
             *
         FROM
-            read_csv_auto ('data/aigineer_yt_2024_2025.csv')
+            read_csv_auto (''/Users/lillmossi/Documents/github/mossad_Hagos_sql_duckdb.ED25/data /aigineer_yt_2024_2025.csv'')
     );
